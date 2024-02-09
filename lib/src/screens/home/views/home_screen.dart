1ce2fe94f284a8d03d215425b2641ee7e3a74553
JuanTime2Late <juan.vera05@utrgv.edu>
2024-02-09 00:40:17 -0600

    homescreen changed

--- lib/src/screens/home/views/home_screen.dart
+++ lib/src/screens/home/views/home_screen.dart
@@ -1,27 +1,44 @@
-
-import 'package:attendance_app/src/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
+//Home screen or Post-Login welcome screen
+import 'package:attendance_app/src/screens/home/views/settings_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int aindex = 1;
+  List<Widget> screens = const [
+    Text('hello for now!'),
+    Text('Home screen'),
+    SettingsScreen(),
+  ];
+  
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Welcome to Auto Attendance'),
-            ElevatedButton(
-              onPressed: () {
-                context.read<SignInBloc>().add(SignOutRequired());
-              },
-              child: const Text('Sign Out'),
-            )
-          ],
-        ),
+      body: Center(child: screens[aindex]),
+      bottomNavigationBar: NavigationBar(
+        height: 60,
+        selectedIndex: aindex,
+        onDestinationSelected: (index) => setState(() => aindex = index),//(index) => setState(() => aindex = index),
+        destinations:const [
+          NavigationDestination(icon: Icon(Icons.map_outlined), 
+          selectedIcon: Icon(Icons.map), 
+          label: 'CAMERA', 
+          ),
+          NavigationDestination(icon: Icon(Icons.home_outlined), 
+          selectedIcon: Icon(Icons.home_filled), 
+          label: 'HOME',
+          ),
+          NavigationDestination(icon: Icon(Icons.settings_outlined), 
+          selectedIcon: Icon(Icons.settings), 
+          label: 'SETTINGS',
+          ),
+        ],
       ),
     );
   }
