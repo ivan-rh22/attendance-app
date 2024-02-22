41965bc20f8a22cce89df840dbc0cc37ef9945bd
Ricardo Ivan Hernandez <ricardo.hernandez21@utrgv.edu>
2024-02-22 11:07:14 -0600

    fix file name

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_test/flutter_test.dart';
+import 'package:test/test.dart';
 // Temporary test to verify that the integration test is running.
 void main() {
   test('integration test', () {
