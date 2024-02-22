8cdd09122dca840447ef97ce68bf187970d64149
Ricardo Ivan Hernandez <ricardo.hernandez21@utrgv.edu>
2024-02-22 10:32:06 -0600

    initial testing env setup

---
+++ integration_test/integration_test.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart';
+// Temporary test to verify that the integration test is running.
+void main() {
+  test('integration test', () {
+    expect(1, 1);
+  });
+}
---
+++ test/unit_test.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart';
+// Temporary test to verify that the unit test is running.
+
+void main() {
+  test('unit test', () {
+    expect(1, 1);
+  });
+}
