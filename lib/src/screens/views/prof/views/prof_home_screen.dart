7f37a2c67dd311e348e5a7943c3b418aa46719c0
Ricardo Ivan Hernandez <ricardo.hernandez21@utrgv.edu>
2024-04-08 13:38:09 -0500

    still broken

--- lib/src/screens/views/prof/views/prof_home_screen.dart
+++ lib/src/screens/views/prof/views/prof_home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:attendance_app/src/screens/views/prof/views/temp_map_screen.dart';
 import 'package:attendance_app/src/blocs/authentication_bloc/authentication_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
